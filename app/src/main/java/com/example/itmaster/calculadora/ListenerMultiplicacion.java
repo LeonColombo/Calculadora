1c3039eaf82037272a819f532b8dff4061c8c92d
LeonColombo <pabloleoncolombo@yahoo.com.ar>
2018-08-02 20:29:25 -0300

    Resta hecha

--- app/src/main/java/com/example/itmaster/calculadora/ListenerMultiplicacion.java
+++ app/src/main/java/com/example/itmaster/calculadora/ListenerMultiplicacion.java
@@ -1,4 +1,23 @@
 package com.example.itmaster.calculadora;
 
-public class ListenerMultiplicacion {
+import android.content.Intent;
+import android.view.View;
+
+public class ListenerMultiplicacion implements View.OnClickListener{
+    private MainActivity context;
+
+    public ListenerMultiplicacion(MainActivity context) {
+        this.context = context;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this.context, ActivityResultado.class);
+        
+
+
+
+    }
 }
--- app/src/main/java/com/example/itmaster/calculadora/ListenerResta.java
+++ app/src/main/java/com/example/itmaster/calculadora/ListenerResta.java
@@ -1,8 +1,25 @@
 package com.example.itmaster.calculadora;
 
+import android.content.Intent;
 import android.view.View;
 
 public class ListenerResta implements View.OnClickListener {
 
     private MainActivity context;
+
+    public ListenerResta(MainActivity context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this.context, ActivityResultado.class);
+
+        Integer resta = Integer.valueOf(context.getNumero1().getText().toString()) - Integer.valueOf(context.getNumero2().getText().toString());
+        intent.putExtra("RESTA", resta);
+
+
+        context.startActivity(intent);
+    }
 }
